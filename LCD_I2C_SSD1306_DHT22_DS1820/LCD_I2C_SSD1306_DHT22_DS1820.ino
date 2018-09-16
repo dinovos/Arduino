1d4434b45dcfcc70e9f3f6d5d8e3a42c737f811e
dinovos <dino.novosel2@zg.t-com.hr>
2018-09-16 12:34:46 +0200

    Change display
    Added button to change view

--- LCD_I2C_SSD1306_DHT22_DS1820/LCD_I2C_SSD1306_DHT22_DS1820.ino
+++ LCD_I2C_SSD1306_DHT22_DS1820/LCD_I2C_SSD1306_DHT22_DS1820.ino
@@ -18,6 +18,9 @@ DallasTemperature sensor(&ourWire);
 float hum;
 float temp;
 float temp1;
+const int button = 4;
+int buttonState = 1;
+int brojac = 0;
 
 void printWelcome(String message) {
   display.setTextSize(2);
@@ -43,9 +46,10 @@ void printWelcome2(String message) {
 }
 
 void getValuesDHT22() {
+  display.setTextSize(1);
   display.setCursor(5, 0);
   display.println("T: ");
-  
+
   display.setCursor(65, 0);
   display.println("Hum: ");
 
@@ -60,22 +64,55 @@ void getValuesDHT22() {
 
 }
 
+void getValuesOnlyDHT22() {
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println("T: ");
+
+  display.setCursor(0, 15);
+  display.println("Hum: ");
+
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  display.setCursor(30, 0);
+  display.println(temp);
+
+  display.setCursor(40, 15);
+  display.println(hum);
+
+}
+
 void getValueExternal() {
+  display.setTextSize(1);
   sensor.requestTemperatures();
   temp1 = sensor.getTempCByIndex(0);
-  display.setCursor(5,15);
+  display.setCursor(5, 15);
   display.println("Ext T: ");
-  display.setCursor(50,15);
+  display.setCursor(50, 15);
+  display.println(temp1);
+}
+
+void getValueExternalOnly() {
+  display.setTextSize(2);
+  sensor.requestTemperatures();
+  temp1 = sensor.getTempCByIndex(0);
+  display.setCursor(5, 0);
+  display.println("Ext probe: ");
+  display.setCursor(5, 15);
   display.println(temp1);
 }
 
 void setup() {
+
+  pinMode(button, INPUT);
+
   Serial.begin(9600);
-  
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   dht.begin();
   sensor.begin();
-  
+
   display.clearDisplay();
   display.display();
 
@@ -84,9 +121,44 @@ void setup() {
 }
 
 void loop() {
-  display.clearDisplay();
-  getValuesDHT22();
-  getValueExternal();
-  display.display();
-  delay(300);
+
+  buttonState = digitalRead(button);
+  Serial.println(buttonState);
+
+  if (buttonState == LOW) {
+    delay(500);
+    brojac++;
+    Serial.println("Brojac povecan za 1: " + brojac);
+  }
+
+  switch (brojac) {
+    case 0:
+      display.clearDisplay();
+      getValuesDHT22();
+      getValueExternal();
+      display.display();
+      Serial.println("Brojac na 0");
+      break;
+    case 1:
+      display.clearDisplay();
+      getValuesOnlyDHT22();
+      display.display();
+      Serial.println("Brojac na 1");
+      break;
+    case 2:
+      display.clearDisplay();
+      getValueExternalOnly();
+      display.display();
+      Serial.println("Brojac na 2");
+      break;
+    case 3:
+      Serial.println("Brojac na 3, vracanje na 0");
+      brojac = 0;
+      break;
+  }
+
+
+
+
+
 }
